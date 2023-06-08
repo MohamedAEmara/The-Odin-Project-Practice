dfadee5b219f759f5c1194debc7d538e4680f890
MohamedAEmara <medoemara104@gmail.com>
2023-06-08 20:54:30 +0300

    finish apply webpack lesson

--- JavaScript/webpack-demo/src/index.js
+++ JavaScript/webpack-demo/src/index.js
@@ -3,8 +3,19 @@ import './style.css';
 import Icon from './icon.png';
 import Data from './data.xml';
 import Notes from './data.csv';
+import toml from './data.toml';
+import yaml from './data.yaml';
+import json from './data.json5';
 
 
+console.log(toml.title);        // output 'TOML Example'
+console.log(toml.owner.name);   // output 'Tom preston-Werner'
+
+console.log(yaml.title);        // 'YAML example"
+console.log(yaml.owner.name);   // 'Tom preston-Werner'
+
+console.log(json.title);        // 'JSON5 Example'
+console.log(json.owner.name);   // 'Tom Preston-Werner'
 
 function component() {
     const element = document.createElement('div');
