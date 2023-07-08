f3955317ae34718dee1ac12c2095418fe9189856
Mohamed Emara <medoemara104@gmail.com>
2023-07-08 11:32:28 +0300

    Add Local Storage code

---
+++ JavaScript/Local-Storage/code.js
@@ -0,0 +1,31 @@
+function setStyles() {
+  const currentColor = localStorage.getItem("bgcolor");
+  const currentFont = localStorage.getItem("font");
+  const currentImage = localStorage.getItem("image");
+
+  document.getElementById("bgcolor").value = currentColor;
+  document.getElementById("font").value = currentFont;
+  document.getElementById("image").value = currentImage;
+
+  htmlElem.style.backgroundColor = `#${currentColor}`;
+  pElem.style.fontFamily = currentFont;
+  imgElem.setAttribute("src", currentImage);
+}
+
+function populateStorage() {
+  localStorage.setItem("bgcolor", document.getElementById("bgcolor").value);
+  localStorage.setItem("font", document.getElementById("font").value);
+  localStorage.setItem("image", document.getElementById("image").value);
+
+  setStyles();
+}
+
+if (!localStorage.getItem("bgcolor")) {
+  populateStorage();
+} else {
+  setStyles();
+}
+
+bgcolorForm.onchange = populateStorage;
+fontForm.onchange = populateStorage;
+imageForm.onchange = populateStorage;
